66ede07c351a2f6f853cef80dfc16eb5fcd94d93
James Ragland <jraglan4@vols.utk.edu>
2019-09-28 18:52:11 -0400

    updated sim

--- Translation_Sims/1Dsim.cpp
+++ Translation_Sims/1Dsim.cpp
@@ -6,15 +6,17 @@
 #include <vector>
 #include <fstream>
 #include <cstdlib>
+#include <ctime>
+#include <fstream>
 using namespace std;
 
 void test_jumper(int cycles)
 {
-	vector <int> rv(10);		// Vector storing location of ribosomes
-	vector <int> mv(10);		// Vector storing location of mRNA molecules
-	int loc0, loc1,dz;			// Integer to store vector index for seeding molecules
+	vector <int> rv(10,0);		// Vector storing location of ribosomes
+	vector <int> mv(10,0);		// Vector storing location of mRNA molecules
+	int loc0,loc1,dz,k,j;		// Integer to store vector index for seeding molecules
 	srand(time(NULL));			// Function invocation to provide pseudo-random numbers
-
+	ofstream r_out;
 
 	for (int i = 0; i < 3; i++){ loc0 = rand() % 10; rv[loc0]++;}
 	for (int m = 0; m < 10; m++){ loc1 = rand() % 10; mv[loc1]++;}
@@ -26,24 +28,25 @@ void test_jumper(int cycles)
 	for (size_t t = 0; t < 10; t++){cout << mv[t] << " ";}
 	cout << endl;
 
-	for (int k = 0; k < cycles; k++){						// Iterate over each lattice site, check if ribosome present
-		for (size_t j = 0; j < 10; j++){					// If present, use psedo-random number to choose left or right hop
-			if (rv[j] != 0){								// Ribosomes at far left or right of volume can only hop
-				dz = rand() % 2;							// One direction
+	for (int k = 0; k < cycles; k++){
+		for (size_t j = 0; j < 10; j++){
+				dz = rand() % 2;	
 				if (dz == 1){
-					if (j != 9){rv[j]--; rv[j+1]++;}
+					if (j != 9 && rv[j] != 0){rv[j]--; rv[j+1]++;}
 				}
 				if (dz == 0){
-					if (j != 0){rv[j]--; rv[j-1]++;}
+					if (j != 0 && rv[j] != 0){rv[j]--; rv[j-1]++;}
 				}
 			}
-		}
-
-		cout << "\n\nStep " << k+1 << " Ribosome Locations\n"; 
-		for(size_t j = 0; j < 10; j++){cout << rv[j] << " ";}
+	}		
+		cout << "\nFinal Ribosome Locations: \n";
+			for(size_t j = 0; j < 10; j++){cout << rv[j] << " ";}
 		cout << endl;
-	}
+		r_out.open("r_out.txt",ios::app);
+			for(size_t j = 0; j < 10; j++){r_out << rv[j] << " ";}
+			r_out << endl;
 }
+
 int main()
 {
 	test_jumper(100);
